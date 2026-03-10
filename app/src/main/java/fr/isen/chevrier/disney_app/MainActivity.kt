a9b6c3fecb26b4b904ae805530f790b872f99f9a
LaraCrafte <clara.langev@gmail.com>
2026-03-10 08:30:30 +0100

    Update MainActivity.kt

--- app/src/main/java/fr/isen/chevrier/disney_app/MainActivity.kt
+++ app/src/main/java/fr/isen/chevrier/disney_app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,17 +23,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            //RegistrationScreen()
-            //LoginScreen()
-            /*DisneyappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }*/
-
             val navController = rememberNavController()
 
             DisneyappTheme {
